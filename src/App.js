0d300d372fa5ae06938c186cf0e75071f4aba025
Stephanie Mitchell <stephsorandom@gmail.com>
2021-02-09 17:02:57 -0500

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
+
+
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App" >
-      </div>
-    );
-  }
+   getContacts() {
+      let contactList = [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]];
+    return contactList;
+    }
+    populateCelebs() {
+        return this.getContacts().map((celeb, i) => {
+            return (<tr>
+            <td><img src={celeb.pictureUrl} alt=""></img> </td>
+            <td>{celeb.name} </td>
+            <td>{celeb.popularity} </td>
+            </tr>)
+        })
+    }
+
+    
+    render() {
+      return (
+        <div className="App" >
+      
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+          {this.populateCelebs()}
+          </tbody>
+        </table>
+        </div>
+      );
+    }
+  
 }
 
 export default App;
